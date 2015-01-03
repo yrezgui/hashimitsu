1f4f1b2ab96aedeb9c71c284f5ef030ddc3f274f
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-03 12:54:38 +0000

    Add main JS file

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var React = window.React = require('react/addons');
+
+var App     = require('./components/App');
+var Loading = require('./components/Loading');
+
+React.render(
+  <App />,
+  document.body
+);
