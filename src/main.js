31d8cb5e383c60203dda5f0648884594f6a4109e
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-05 02:42:30 +0000

    Fetch remote API and cached results in LocalStorage when the app is loaded

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 var React = window.React = require('react/addons');
 
+var ApiUtils = require('./utils/ApiUtils');
+
 var App     = require('./components/App');
 var Loading = require('./components/Loading');
+
+
+ApiUtils.localFetchJobs();
+ApiUtils.remoteFetchJobs();
 
 React.render(
   <App />,
