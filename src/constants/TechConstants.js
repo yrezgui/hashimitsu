dc6803ff641d3fde4e7fed070cefb54f03d06108
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-05 02:41:08 +0000

    Add Technologies constants

---
+++ src/constants/TechConstants.js
@@ -0,0 +1,81 @@
+module.exports = {
+  DEFAULT: {
+    color: '#ddd', //7b8994
+    symbol: 'fa-file'
+  },
+  PHP: {
+    alias: ['php', 'composer', 'zend', 'symphony', 'cakephp'],
+    color: '#6666b9',
+    symbol: 'icon-php'
+  },
+  HTML: {
+    alias: ['html', 'xhtml', 'html5', 'phonegap', 'cordova', 'chrome'],
+    color: '#f0662b',
+    symbol: 'fa fa-html5'
+  },
+  JAVA: {
+    alias: ['java', 'jsp'],
+    color: '#b05209',
+    symbol: 'icon-java-bold'
+  },
+  DART: {
+    alias: ['dart'],
+    color: '#98BAD6',
+    symbol: 'icon-google-developers'
+  },
+  NODEJS: {
+    alias: ['nodejs', 'express', 'node-webkit', 'mean'],
+    color: '#f09549',
+    symbol: 'icon-nodejs'
+  },
+  PYTHON: {
+    alias: ['python', 'pip', 'django', 'flask'],
+    color: '#3581ba',
+    symbol: 'icon-python'
+  },
+  RUBY: {
+    alias: ['ruby', 'ror', 'ruby-on-rails', 'gem', 'sinatra'],
+    color: '#dc5647',
+    symbol: 'icon-ruby'
+  },
+  GO: {
+    alias: ['go'],
+    color: '#7d54cb',
+    symbol: 'icon-google'
+  },
+  CPLUSPLUS: {
+    alias: ['c', 'c++'],
+    color: '#f36b8d',
+    symbol: 'icon-cplusplus'
+  },
+  LUA: {
+    alias: ['lua'],
+    color: '#aa5af1',
+    symbol: 'icon-moon'
+  },
+  CLOJURE: {
+    alias: ['clojure'],
+    color: '#db5855',
+    symbol: 'icon-clojure'
+  },
+  SCALA: {
+    alias: ['scala'],
+    color: '#6dc3a0',
+    symbol: 'icon-scala'
+  },
+  METEOR: {
+    alias: ['meteor'],
+    color: '#244776',
+    symbol: 'icon-fire-alt'
+  },
+  ANDROID: {
+    alias: ['android'],
+    color: '#A4C639',
+    symbol: 'fa fa-android'
+  },
+  IOS: {
+    alias: ['ios', 'objective-c', 'swift', 'iphone', 'ipad'],
+    color: '#F88F41',
+    symbol: 'fa fa-apple'
+  }
+};
